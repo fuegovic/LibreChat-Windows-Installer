9310dad9e025759100baf856af8cf7426b6d6da5
Fuegovic <32828263+fuegovic@users.noreply.github.com>
2023-04-15 19:44:48 -0400

    Update Copy-TemplateBatFile.psm1
    bug fix and cleanup

--- Modules/Copy-TemplateBatFile.psm1
+++ Modules/Copy-TemplateBatFile.psm1
@@ -1,83 +1,159 @@
-<#
-.SYNOPSIS
-This PowerShell module contains a function called "Copy-TemplateBatFile" that copies a template batch file from one location to another, replaces certain variables within the batch file with the correct values, and saves the updated batch file.
+<# 
+This script automates the local installation of ChatGPT-Clone for Windows. 
 
-.DESCRIPTION
-This PowerShell module is used for creating a batch file based on a template. It includes a function called "Copy-TemplateBatFile" that copies a batch file template from one location to another location and customizes certain variables within the batch file.
+ChatGPT-Clone is a web app that lets you use ChatGPT and Bing with custom parameters.
+The Github link is https://github.com/danny-avila/chatgpt-clone
 
-.PARAMETER
-original_location: The path of the directory containing the template batch file to be copied.
+You can provide a config.json file with the following keys:
+MONGO_URI: The connection string for MongoDB
+OPENAI_KEY: The API key for OpenAI
+BINGAI_TOKEN: The access token for Bing
 
-final_dir: The path of the directory where the copied batch file will be saved.
+Alternatively, you can pass the same values as named parameters when running the script.
+For example: .\main.ps1 -mongo_uri "mongodb://localhost:27017" -openai_key "sk-123456789" -bingai_token "abcdefg"
 
-envfile: The path of the file that contains variables that should be replaced in the final batch file. Specifically, this file contains the value for the MEILI_MASTER_KEY variable.
+The use of either the config file or the parameters is optional.
+
+This script was made by Fuegovic (https://github.com/fuegovic), a amateur developer and a musician/producer. You can check out his music on Bandcamp (https://fuegovic.bandcamp.com) or Spotify (https://open.spotify.com/artist/3ZfaxdODbE1NrfQYsGO92R)
 #>
-
-function Copy-TemplateBatFile {
-param (
-    [string]$original_location,
-    [string]$final_dir, 
-    [string]$envfile
+# Init
+# Define the parameters
+Param (
+  [switch]$Debug,
+  [string]$mongo_uri,
+  [string]$openai_key,
+  [string]$bingai_token
 )
-Write-Host "***  Create ChatGPT-Clone.bat, used to Start and Update the project ***" -ForegroundColor Blue
-Write-Host "`n"
 
-# Set the paths to the template batch file and the destination for the copied batch file
-$template_file = "$original_location/Dependencies/template.bat"
-$copied_file = "$final_dir\ChatGPT-Clone.bat"
-
-# Copy the template batch file to the destination directory
-Copy-Item $template_file $copied_file
-
-# Read in the contents of the copied batch file
-$batch_contents = Get-Content $copied_file
-
-# Replace the $final_dir and $MEILI_MASTER_KEY variables with the correct values
-$batch_contents = $batch_contents -replace '\$final_dir', $final_dir
-$MEILI_MASTER_KEY = (Get-Content -Path $envfile) -notmatch "^#" -match "MEILI_MASTER_KEY=" | ForEach-Object {$_ -replace 'MEILI_MASTER_KEY=', ''}
-$batch_contents = $batch_contents -replace '\$MEILI_MASTER_KEY', $MEILI_MASTER_KEY
-
-# Save the updated contents to the copied batch file
-Set-Content $copied_file $batch_contents
-
-Copy-Item "$original_location\Dependencies\ChatGPT-Clone.ico" "$final_dir\ChatGPT-Clone.ico"
-
-# Check to see if the desktop is in OneDrive, in the user's home directory, or both
-$OneDrive = "$env:USERPROFILE\OneDrive\Desktop"
-$UserDesktop = "$env:HOMEPATH\Desktop"
-if ((Test-Path $OneDrive) -and (Test-Path $UserDesktop)) {
-    Write-Host "The desktop is located in both OneDrive and $env:HOMEPATH." -ForegroundColor Cyan
-    $Location = Read-Host "Which directory would you like to copy the shortcut to? (OneDrive, Home)"
-    If ($Location -eq "OneDrive"){
-        $shortcutLocation = "$OneDrive"
-    }
-    Else{
-        $shortcutLocation = "$UserDesktop"
-    }
-}
-elseif (Test-Path $OneDrive){
-    $shortcutLocation = "$OneDrive"
-}
-elseif(Test-Path $UserDesktop){
-    $shortcutLocation = "$UserDesktop"
-}
-else{
-    $shortcutLocation = "$final_dir"
-}
-
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$shortcutLocation\ChatGPT-Clone.lnk")
-$Shortcut.TargetPath = "$final_dir\ChatGPT-Clone.bat"
-$Shortcut.IconLocation = "$final_dir\ChatGPT-Clone.ico"
-$Shortcut.WorkingDirectory = "$final_dir"
-
-$Shortcut.Save()
-Write-Host "The shortcut to ChatGPT-Clone is : $shortcutLocation\ChatGPT-Clone.lnk" -ForegroundColor Cyan
-
-# Pause and clear the screen
-Write-Host "`nPress any key to continue..." -ForegroundColor Magenta
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+# Clear all Variables, Modules, and the console window
+Remove-Variable * -ErrorAction SilentlyContinue; Remove-Module *; 
+$error.Clear(); 
 Clear-Host
 
-return $shortcutLocation
+if ($Debug) {
+  # Enable debug output for this module
+  $DebugPreference = 'Continue'
 }
+
+# Get the current location and assign it to a variable
+$original_location = Get-Location
+
+# Start Parameters Check
+# Check if the user provided all the parameters or some of them or none of them
+if ($PSBoundParameters.Count -eq 3) {
+  # Use all the parameters
+} elseif ($PSBoundParameters.Count -gt 0) {
+  
+  # Use some of the parameters and ask for the rest
+  if ($PSBoundParameters.ContainsKey("mongo_uri")) {
+  }
+  if ($PSBoundParameters.ContainsKey("openai_key")) {
+  }
+  if ($PSBoundParameters.ContainsKey("bingai_token")) {
+  }
+}
+
+# Import all the modules
+Import-Module .\Modules\Get-IniContent.psm1
+Import-Module .\Modules\Get-InstallDirectory.psm1
+Import-Module .\Modules\Install-GitSoftware.psm1
+Import-Module .\Modules\Copy-GitRepository.psm1
+Import-Module .\Modules\Install-NodeJS.psm1
+Import-Module .\Modules\New-EnvironmentFile.psm1
+Import-Module .\Modules\Install-MeiliSearch.psm1
+Import-Module .\Modules\Get-MongoURI.psm1
+Import-Module .\Modules\Get-OpenAIAPIKey.psm1
+Import-Module .\Modules\Get-BingAccessToken.psm1
+Import-Module .\Modules\Install-Ngrok.psm1
+Import-Module .\Modules\Invoke-NpmCommands.psm1
+Import-Module .\Modules\Copy-TemplateBatFile.psm1
+Import-Module .\Modules\Show-InstallationConclusion.psm1
+
+# Check if there is a config.ini file and read it
+if (Test-Path .\config.ini) {
+  $config = Get-IniContent -Path .\config.ini
+  # Check if the parameters are missing or invalid and use the values from the config file
+  if (-not $PSBoundParameters.ContainsKey("mongo_uri") -or -not $mongo_uri) {
+    $mongo_uri = $config.Database.mongo_uri
+  }
+  if (-not $PSBoundParameters.ContainsKey("openai_key") -or -not $openai_key) {
+    $openai_key = $config.API.openai_key
+  }
+  if (-not $PSBoundParameters.ContainsKey("bingai_token") -or -not $bingai_token) {
+    $bingai_token = $config.API.bingai_token
+  }
+}
+
+# Install requirements (Git and Node.js)
+# Install Git
+$gitExitRequired = [bool] (Install-GitSoftware)
+
+# Install Node.js
+$nodeExitRequired = [bool] (Install-NodeJS)
+
+
+if ($gitExitRequired -or $nodeExitRequired -eq $true){
+  Write-Host "Some requirements have just been installed, you will have to restart the install for them to take effects."  -ForegroundColor Red
+  Write-Host "Please press any key to exit..." -ForegroundColor Red
+  $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')	
+  Exit
+}
+
+# Install Directory, Copy Repository, Create .env File
+# Run Get-InstallDirectory
+$final_dir = Get-InstallDirectory
+
+# Run Copy-GitRepository with the final directory as an argument
+Copy-GitRepository $final_dir
+
+# Create a New .env File
+$template = "$final_dir\api\.env.example"
+$envfile = "$final_dir\api\.env"
+Invoke-Command -ScriptBlock {New-EnvironmentFile $template $envfile $openai_key $bingai_token $mongo_uri}
+
+# Soft Requirements (MeiliSearch and ngrok)
+# Downloading Meilisearch
+#Install-MeiliSearch -final_dir $final_dir -envfile $envfile
+
+# Run Install-Ngrok
+#Install-Ngrok $final_dir 
+
+# Instruction for MongoDB, OpenAI, Bing and ngrok
+# If the mongo_uri parameter is missing or invalid, run Get-MongoURI
+if (-not $mongo_uri) {
+  Get-MongoURI $envfile $final_dir
+}
+
+# If the openai_key parameter is missing or invalid, run Get-MongoURI
+if (-not $openai_key) {
+  Get-OpenAIAPIKey $envfile $final_dir
+}
+
+# If the bingai_token parameter is missing or invalid, run Get-MongoURI
+if (-not $bingai_token) {
+  Get-BingAccessToken $envfile $final_dir
+}
+
+# Run Get-IniContent
+Import-Module .\Modules\Get-IniContent.psm1
+
+# Build Project, Create .bat File, Conclusion
+
+# Run Invoke-NpmCommands
+#Invoke-NpmCommands $final_dir
+
+# Run Copy-TemplateBatFile
+$InformationPreference = 'Continue'
+$shortcutLocation = Copy-TemplateBatFile $original_location $final_dir $envfile
+
+# Run Show-InstallationConclusion
+Show-InstallationConclusion $final_dir $shortcutLocation
+
+# Clean Up and Exit
+# Clear all Variables, Modules, and the console window
+Remove-Variable * -ErrorAction SilentlyContinue; Remove-Module *; 
+$error.Clear(); 
+Clear-Host
+
+# Exit the script with a success code
+Exit 0
